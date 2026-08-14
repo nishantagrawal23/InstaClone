1e25ea29dd281274a1ba6ed7a98e7f28b8bc5a42
nishantagrawal23 <nishant.agrawal@thoughtwin.com>
2026-08-14 11:52:57 +0530

    complete

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -25,37 +25,37 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     private readonly jwtService: JwtService,
     private readonly chatService: ChatService,
     private readonly mailService: MailService,
-  ) {}
+  ) { }
 
   @WebSocketServer()
   server!: Server;
 
   private onlineUsers = new Map<string, Set<string>>();
-handleConnection(client: Socket) {
-  try {
-   
+  handleConnection(client: Socket) {
+    try {
 
-    const token = client.handshake.auth.token;
 
-    const payload = this.jwtService.verify(token, {
-      secret: process.env.JWT_ACCESS_SECRET,
-    });
+      const token = client.handshake.auth.token;
 
-    client.data.userId = payload.id;
+      const payload = this.jwtService.verify(token, {
+        secret: process.env.JWT_ACCESS_SECRET,
+      });
 
-    const userId = payload.id;
+      client.data.userId = payload.id;
 
-    if (!this.onlineUsers.has(userId)) {
-      this.onlineUsers.set(userId, new Set());
+      const userId = payload.id;
+
+      if (!this.onlineUsers.has(userId)) {
+        this.onlineUsers.set(userId, new Set());
+      }
+
+      this.onlineUsers.get(userId)?.add(client.id);
+
+
+    } catch (err) {
+      console.error(err);
     }
-
-    this.onlineUsers.get(userId)?.add(client.id);
-
-    
-  } catch (err) {
-    console.error(err);
   }
-}
 
   handleDisconnect(client: Socket) {
     for (const [userId, sockets] of this.onlineUsers.entries()) {
@@ -68,44 +68,44 @@ handleConnection(client: Socket) {
   }
 
   @SubscribeMessage('send_message')
-async handleMessage(
-  @ConnectedSocket() client: Socket,
-  @MessageBody() dto: SendMessageDto,
-) {
-  const senderId = client.data.userId;
+  async handleMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() dto: SendMessageDto,
+  ) {
+    const senderId = client.data.userId;
 
-  const savedMessage = await this.chatService.sendMessage(senderId, dto);
+    const savedMessage = await this.chatService.sendMessage(senderId, dto);
 
-  // Send to receiver
-  const receiverSockets = this.onlineUsers.get(dto.receiverId);
+    // Send to receiver
+    const receiverSockets = this.onlineUsers.get(dto.receiverId);
 
-  if (receiverSockets) {
-  // Receiver is online
-  receiverSockets.forEach((socketId) => {
-    this.server.to(socketId).emit(
-      'receive_message',
-      savedMessage,
-    );
-  });
-} else {
-  // Receiver is offline
-  const receiverEmail = await this.chatService.getUserEmail(
-    dto.receiverId,
-  );
+    if (receiverSockets) {
+      // Receiver is online
+      receiverSockets.forEach((socketId) => {
+        this.server.to(socketId).emit(
+          'receive_message',
+          savedMessage,
+        );
+      });
+    } else {
+      // Receiver is offline
+      const receiverEmail = await this.chatService.getUserEmail(
+        dto.receiverId,
+      );
 
-  await this.mailService.sendMessageNotification(
-    receiverEmail,
-    savedMessage!.sender.name,
-  );
-}
+      await this.mailService.sendMessageNotification(
+        receiverEmail,
+        savedMessage!.sender.name,
+      );
+    }
 
-// Send back to sender
-client.emit('receive_message', savedMessage);
-  return {
-    success: true,
-    message: savedMessage,
-  };
-}
+    // Send back to sender
+    client.emit('receive_message', savedMessage);
+    return {
+      success: true,
+      message: savedMessage,
+    };
+  }
 }
 
 
